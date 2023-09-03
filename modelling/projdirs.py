445633379deee23ce1d051e86a27681e94f384f3
ahmadmojiri <98721000+ahmadmojiri@users.noreply.github.com>
2023-09-04 09:59:31 +1000

    Recreate projdirs.py
    Also debugged the jupyter file to be able to run from this new location removing its hard coding

---
+++ modelling/projdirs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  4 09:22:17 2023
+
+@author: Ahmad Mojiri
+"""
+
+import os
+ 
+
+basedir = os.path.realpath('..')
+
+datadir = basedir + "\data\\"
+modeldir = basedir + "\modelling\python\package\\"
+optdir = basedir + "\modelling\minizinc\\"
+figdir = basedir + "\modelling\figures\\"
+resultsdir = datadir + "\arbitrage\\"
